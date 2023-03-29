f373c6ebb6ad2ae9074903daa642da8d1e1442df
Vedran Josipovic <vedran.josipovic@verkstedt.com>
2023-03-29 13:25:10 +0200

    remove time stuff

--- triggers/grab_topics_trigger.ts
+++ triggers/grab_topics_trigger.ts
@@ -1,21 +1,13 @@
 import { Trigger } from "deno-slack-api/types.ts";
 import { TriggerTypes } from "deno-slack-api/typed-method-types/workflows/triggers/mod.ts";
 
-const increaseTime = (secondsOffset = 20, hoursOffset = -2) => {
-  const currentTime = new Date();
-  currentTime.setSeconds(currentTime.getSeconds() + secondsOffset);
-  currentTime.setHours(currentTime.getHours() + hoursOffset);
-
-  return d.toISOString();
-};
-
 const grabTopicsSelectionTrigger: Trigger = {
   name: "Grab Topics",
   type: TriggerTypes.Scheduled,
   workflow: "#/workflows/grab_topics_workflow", // Replace with custom function
   inputs: {},
   schedule: {
-    start_time: increaseTime(),
+    start_time: "2023-03-29T09:011:27.820Z",
     timezone: "UTC",
     // frequency: {
     //   type: "weekly",
