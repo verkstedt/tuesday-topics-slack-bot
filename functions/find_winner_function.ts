bfb198a7fc6328eab45418b5b4f7740333bd551e
Vedran Josipovic <vedran.josipovic@verkstedt.com>
2023-05-24 16:38:53 +0200

    chore(datastore): add find winner logic

--- functions/find_winner_function.ts
+++ functions/find_winner_function.ts
@@ -15,7 +15,7 @@ export const FindWinnerFunctionDefinition = DefineFunction({
   input_parameters: {
     properties: {
       channelId: {
-        type: Schema.types.string,
+        type: Schema.slack.types.channel_id,
         description: "Channel ID ",
       },
     },
@@ -38,17 +38,75 @@ export default SlackFunction(
   async ({ inputs, client }) => {
     const { channelId } = inputs;
 
-    const response = await client.apps.datastore.query({
-      datastore: "suggestions",
-      expression: "#wasWinner = :value",
-      expression_attributes: { "#wasWinner": "wasWinner" },
-      expression_values: { ":value": 0 },
+    // const reponse = await client.apps.datastore.query({
+    //   datastore: "suggestions",
+    //   expression: "#wasWinner = :value",
+    //   expression_attributes: {"#wasWinner": "wasWinner"},
+    //   expression_values: { ":value": 0 },
+    // });
+
+    // const validSuggestions = reponse.items?.filter((item) => item.text);
+     
+    // // console.log({validSuggestions});
+
+    const history = await client.conversations.history({
+      channel: channelId,
+    });
+    
+    // Get the latest poll
+    const pollMessage = history.messages?.filter((message) => {
+      return message?.text.includes("Topics for Tuesday");
+    })[0];
+
+    // Get ID of pollMessage
+    // Send request to get thread
+    //
+    const pollMessageReplies = await client.conversations.replies({
+      channel: channelId,
+      ts: pollMessage.ts,
     });
 
-    const validSuggestions = response.items?.filter((item) => item.text);
+    const pollSuggestions = pollMessage.text.split(
+      "*Topics for Tuesday*",
+    )[1]
+      .split("\n").filter((str) => Boolean(str.trim()));
 
-    console.log({ validSuggestions });
+    console.log({ pollSuggestions });
+    const votes = pollMessageReplies.messages.reduce((results, value) => {
+      if (!results[value.text]) {
+        results[value.text] = 1;
+      } else {
+        results[value.text] += 1;
+      }
 
-    return;
+      return results;
+    }, {});
+
+    const winnerIndex = Object.keys(votes).reduce((a, b) =>
+      votes[a] > votes[b] ? a : b
+    );
+
+    const winnerText = pollSuggestions.find((suggestion) =>
+      suggestion.includes(`${winnerIndex}.`)
+    );
+
+    const winnerSuggestion = (await client.apps.datastore.query({
+      datastore: "suggestions",
+      expression: "#text = :text",
+      expression_attributes: {"#text": "text"},
+      expression_values: { ":text": winnerText },
+    })).items[0];
+
+    await client.apps.datastore.update({datastore: 'suggestion', item: {id: winnerSuggestion.id, wasWinner: 1} });
+
+    const response = await client.chat.postMessage({
+      channel: channelId,
+      text: `
+      *The winner is...*
+      ${winnerSuggestion.text}
+      `,
+    });
+
+    return {}
   },
 );
