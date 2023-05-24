c39f554e65e74531d84db6f7c187d886118914c7
Vedran Josipovic <vedran.josipovic@verkstedt.com>
2023-05-24 10:27:11 +0200

    chore(datastore): include ds infra

---
+++ datastores/suggestions.ts
@@ -0,0 +1,16 @@
+// datastores/good_tunes_datastore.ts 
+import { DefineDatastore, Schema } from "deno-slack-sdk/mod.ts";
+
+export const SuggestionsDatastore = DefineDatastore({
+  name: "suggestions",
+  primary_key: "id",
+  attributes: {
+    id: { type: Schema.types.string },
+    suggestion: { type: Schema.types.string },
+    // suggester: { type: Schema.types.string },
+    createdAt: { type: Schema.types.string },
+    currentEmote: { type: Schema.types.string },
+    currentVoteCount: { type: Schema.types.number },
+    wasWinner: { type: Schema.types.boolean },
+  },
+});
