31497934db7d493120d723e4c74f12f175b1a221
zachverkstedt <zach.townsend@verkstedt.com>
2023-06-19 21:03:30 +0200

    Update find winner name

--- triggers/find_winner_trigger.ts
+++ triggers/find_winner_trigger.ts
@@ -2,10 +2,10 @@ import { Trigger } from "deno-slack-api/types.ts";
 import { TriggerTypes } from "deno-slack-api/typed-method-types/workflows/triggers/mod.ts";
 
 const findWinnerTrigger: Trigger = {
-  name: "Grab Topics",
+  name: "Find Winner",
   type: TriggerTypes.Shortcut,
   workflow: "#/workflows/find_winner_workflow", // Replace with custom function
-  inputs: {},
+  // inputs: {},
   // schedule: {
   //   start_time: "2023-01-25T14:59:30Z",
   //   timezone: "UTC",
