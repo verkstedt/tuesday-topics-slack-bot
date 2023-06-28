062d22c90729213bfb82a345b8a08e8198c9b9bc
zachverkstedt <zach.townsend@verkstedt.com>
2023-06-28 09:20:05 +0200

    Update name

--- manifest.ts
+++ manifest.ts
@@ -9,7 +9,7 @@ import AddSuggestionWorkflow from "./workflows/add_suggestion_workflow.ts";
  * https://api.slack.com/future/manifest
  */
 export default Manifest({
-  name: "tuesday-topics",
+  name: "tuesday-topics-poll-bot",
   description:
     "A sample that demonstrates using a function, workflow and trigger to send a greeting",
   icon: "assets/default_new_app_icon.png",
